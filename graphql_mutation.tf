65852ffeee26ad20766af90825a38cb505f13368
Dima Unterov <dimaunterov@gmail.com>
2025-08-28 10:02:18 +0100

    feat: add example

--- graphql_mutation.tf
+++ graphql_mutation.tf
@@ -2,11 +2,10 @@
 # GraphQL Mutation: Dependency Proxy Packages Settings
 # Ref: https://docs.gitlab.com/api/graphql/reference/#mutationupdatedependencyproxypackagessettings
 # Ref: https://docs.gitlab.com/api/graphql/reference/#dependencyproxypackagessetting
-# - 
 # ---------------------------------------------------------
 
 resource "graphql_mutation" "dependency_proxy_packages_setting" {
-  # Variables, that will be used in GraphQL mutations
+  # Variables that will be used in GraphQL mutations
   mutation_variables = {
     enabled                  = var.enabled
     mavenExternalRegistryUrl = var.maven_external_registry_url
@@ -18,7 +17,7 @@ resource "graphql_mutation" "dependency_proxy_packages_setting" {
     mavenExternalRegistryUrl = "data.updateDependencyProxyPackagesSettings.dependencyProxyPackagesSetting.mavenExternalRegistryUrl"
   }
 
-  # https://registry.terraform.io/providers/sullivtr/graphql/latest/docs/resources/mutation#compute_from_create-1
+  # See https://registry.terraform.io/providers/sullivtr/graphql/latest/docs/resources/mutation#compute_from_create-1
   compute_from_create = true
 
   create_mutation = <<-EOT
@@ -40,8 +39,8 @@ resource "graphql_mutation" "dependency_proxy_packages_setting" {
     }
   EOT
 
-  # In case of updateDependencyProxyPackagesSettings, the same mutation used for both
-  # creation and motification of proxy settings
+  # The same mutation (updateDependencyProxyPackagesSettings) used for both
+  # create_mutation and update_mutation.
   update_mutation = <<-EOT
     mutation UpdateDependencyProxyPackagesSettings(
       $enabled: Boolean!, 
@@ -61,9 +60,10 @@ resource "graphql_mutation" "dependency_proxy_packages_setting" {
     }
   EOT
 
-  # delete_mutation is a mutation that will be run on resource deletion (e.g. terraform destroy).
-  # The trick is, that's the same mutation updateDependencyProxyPackagesSettings, but with hardcoded
-  # `enabled: false` parameter.
+  # delete_mutation is a mutation that will be run on resource deletion (e.g. on terraform destroy).
+  # The trick is that's the same mutation updateDependencyProxyPackagesSettings, but with hardcoded
+  # `enabled: false` parameter. So effectivelly, resource deletion will turn off
+  # the Dependency proxy for packages in GitLab project's settings.
   delete_mutation = <<-EOT
     mutation DeleteDependencyProxyPackagesSettings() {
       updateDependencyProxyPackagesSettings(input: {
@@ -78,7 +78,7 @@ resource "graphql_mutation" "dependency_proxy_packages_setting" {
     }
   EOT
 
-  # read query is used to query the api resource after it has been created.
+  # read_query is used to query the api resource after it has been created.
   read_query = <<-EOT
     query {
       project(fullPath: "${var.full_path}") {
